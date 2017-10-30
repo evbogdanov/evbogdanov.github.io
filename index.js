0ca5f56b495391271dd2641beceeba678d809335
Ev Bogdanov <evbogdanov.com@gmail.com>
2017-10-30 23:53:55 +0300

    Format js

--- index.js
+++ index.js
@@ -16,24 +16,24 @@ function readShortcuts(callback) {
 function handleKey(key, shortcuts, elLink, elSearch) {
     const isEnter = key === KEY_ENTER,
           words = elSearch.value.split(' ')
-	let link = ''
-	if (words.length >= 2) {
-		const shortcut = words[0]
-		words.shift()
-		if (shortcuts.hasOwnProperty(shortcut)) {
-			const searchQuery = encodeURIComponent(words.join(' '))
-			link = shortcuts[shortcut].replace(REPLACE, searchQuery)
-		}
-	}
-	elLink.innerText = link
-	elLink.href = link
-	if (isEnter && link)
+    let link = ''
+    if (words.length >= 2) {
+        const shortcut = words[0]
+        words.shift()
+        if (shortcuts.hasOwnProperty(shortcut)) {
+            const searchQuery = encodeURIComponent(words.join(' '))
+            link = shortcuts[shortcut].replace(REPLACE, searchQuery)
+        }
+    }
+    elLink.innerText = link
+    elLink.href = link
+    if (isEnter && link)
         window.open(link)
 }
 
 function main() {
     const elLink = document.getElementById('link-in'),
-		  elSearch = document.getElementById('search')
+          elSearch = document.getElementById('search')
     readShortcuts(function (shortcuts) {
         if (!shortcuts) {
             console.error('No shortcuts')
