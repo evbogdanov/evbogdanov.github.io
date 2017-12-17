c65fc846501e070aa1c386c0469ec2d4c5410a6b
Ev Bogdanov <evbogdanov.com@gmail.com>
2017-12-17 20:34:56 +0300

    Add dismiss button

--- search/index.js
+++ search/index.js
@@ -3,46 +3,59 @@ const URL_SHORTCUTS = 'shortcuts.json',
       REPLACE = '{{s}}'
 
 function readShortcuts(callback) {
-    const request = new XMLHttpRequest()
-    request.open('GET', URL_SHORTCUTS, true)
-    request.onload = function () {
-        if (this.status === 200)
-            return callback(JSON.parse(this.response))
-        return callback(null)
-    }
-    request.send()
+  const request = new XMLHttpRequest()
+  request.open('GET', URL_SHORTCUTS, true)
+  request.onload = function () {
+    if (this.status === 200)
+      return callback(JSON.parse(this.response))
+    return callback(null)
+  }
+  request.send()
 }
 
-function handleKey(key, shortcuts, elLink, elSearch) {
-    const isEnter = key === KEY_ENTER,
-          words = elSearch.value.split(' ')
-    let link = ''
-    if (words.length >= 2) {
-        const shortcut = words[0]
-        words.shift()
-        if (shortcuts.hasOwnProperty(shortcut)) {
-            const searchQuery = encodeURIComponent(words.join(' '))
-            link = shortcuts[shortcut].replace(REPLACE, searchQuery)
-        }
+function handleKey(key, shortcuts, elLink, elSearch, elDismiss) {
+  const isEnter = key === KEY_ENTER,
+        searchText = elSearch.value,
+        words = searchText.split(' ')
+  let link = ''
+
+  if (searchText)
+    elDismiss.style.display = 'block'
+  else
+    elDismiss.style.display = 'none'
+
+  if (words.length >= 2) {
+    const shortcut = words[0]
+    words.shift()
+    if (shortcuts.hasOwnProperty(shortcut)) {
+      const searchQuery = encodeURIComponent(words.join(' '))
+      link = shortcuts[shortcut].replace(REPLACE, searchQuery)
     }
-    elLink.innerText = link
-    elLink.href = link
-    if (isEnter && link)
-        window.open(link)
+  }
+  elLink.innerText = link
+  elLink.href = link
+  if (isEnter && link)
+    window.open(link)
 }
 
 function main() {
-    const elLink = document.getElementById('link-in'),
-          elSearch = document.getElementById('search')
-    readShortcuts(function (shortcuts) {
-        if (!shortcuts) {
-            console.error('No shortcuts')
-            return
-        }
-        elSearch.addEventListener('keyup', function (ev) {
-            handleKey(ev.which, shortcuts, elLink, elSearch)
-        })
+  const elLink = document.getElementById('link-in'),
+        elSearch = document.getElementById('search'),
+        elDismiss = document.getElementById('dismiss')
+  readShortcuts(function (shortcuts) {
+    if (!shortcuts) {
+      console.error('No shortcuts')
+      return
+    }
+    elSearch.addEventListener('keyup', function (ev) {
+      handleKey(ev.which, shortcuts, elLink, elSearch, elDismiss)
     })
+    elDismiss.addEventListener('click', function () {
+      elSearch.value = ''
+      elLink.innerText = ''
+      elDismiss.style.display = 'none'
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', main)
